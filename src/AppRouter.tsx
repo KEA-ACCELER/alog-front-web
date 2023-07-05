5e9bd10274d9116dabce419531da65a23ac757e4
masibasi <jimin_lee98@naver.com>
2023-07-05 16:09:31 +0900

    feat : create top nav bar prototype

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Page1 } from "./pages/Page1";
 import { Page2 } from "./pages/Page2";
+import { TopNavBar } from "./navigation/TopNavBar";
 
 const AppRouter = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route index element={<Page1 />} />
-                <Route path="/page2" element={<Page2 />} />
+                <Route path="/" element={<TopNavBar />}>
+                    <Route index element={<Page1 />} />
+                    <Route path="/page2" element={<Page2 />} />
+                </Route>
             </Routes>
         </BrowserRouter>
     );
