8401a486773a124d11aad0f947e892c6fffec134
masibasi <jimin_lee98@naver.com>
2023-07-27 12:45:33 +0900

    feat : 전체 데이터를 하나의 data state로 관리

--- src/pages/ReleaseNote/CreateReleaseNote.tsx
+++ src/pages/ReleaseNote/CreateReleaseNote.tsx
@@ -1,78 +1,91 @@
-import { Button } from "react-bootstrap";
+import { Button, Fade } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import "./CreateReleaseNote.css";
 import { RNBadge } from "../../components/RNBadge";
 import { FloatingWrapper } from "../../components/FloatingWrapper";
 import { RNColumn } from "../../components/RNColumn";
-import { RNTag, ReleaseNoteColumnData } from "../../interfaces/releaseNote.interface";
-import { useState } from "react";
+import { RNTag, ReleaseNoteColumnData, ReleaseNoteData } from "../../interfaces/releaseNote.interface";
+import { useRef, useState } from "react";
+import FadeIn from "../../animation/FadeIn";
 
-const mock: ReleaseNoteColumnData[] = [
-    { key: 0, tag: "new", show: true },
-    { key: 1, tag: "featured", show: false },
-    { key: 2, tag: "changed", show: true },
-    { key: 3, tag: "fixed", show: false },
-    { key: 4, tag: "deprecated", show: false },
-    { key: 5, tag: "bug", show: false },
-];
+const mockRNData: ReleaseNoteData = {
+    version: "0.0.0",
+    date: "2999.01.01",
+    content: [
+        { key: 0, content: [{ key: 0, content: "" }], tag: "new", show: false },
+        { key: 1, content: [{ key: 0, content: "" }], tag: "featured", show: false },
+        { key: 2, content: [{ key: 0, content: "" }], tag: "changed", show: false },
+        { key: 3, content: [{ key: 0, content: "" }], tag: "fixed", show: false },
+        { key: 4, content: [{ key: 0, content: "" }], tag: "deprecated", show: false },
+        { key: 5, content: [{ key: 0, content: "" }], tag: "bug", show: false },
+    ],
+};
 
 export const CreateReleaseNote = () => {
     const navigation = useNavigate();
-    const [data, setData] = useState(mock);
+    const [data, setData] = useState(mockRNData);
+    const [categoryData, setCategoryData] = useState(mockRNData.content);
 
     const toggleTag = (tag: RNTag) => {
-        const filteredData = data.map((it) => (it.tag === tag ? { ...it, show: !it.show } : it));
-        console.log(filteredData);
+        let filteredData = data;
+        const filteredContentData = data.content.map((it) => (it.tag === tag ? { ...it, show: !it.show } : it));
+        console.log(filteredContentData);
+        setCategoryData(filteredContentData);
+        filteredData.content = filteredContentData;
         setData(filteredData);
     };
     return (
         <div className="CreateReleaseNote">
-            <div className="mainContainer">
-                <div className="topWrapper">
-                    <h1>Create New Release Note</h1>
-                    <div className="btnWrapper">
-                        <Button className="backBtn" variant="outline-primary" onClick={() => navigation(-1)}>
-                            Back
-                        </Button>
-                        <Button className="saveBtn" variant="outline-primary" onClick={() => navigation("")}>
-                            Save
-                        </Button>
+            <FadeIn width={"100%"}>
+                <div className="mainContainer">
+                    <div className="topWrapper">
+                        <h1>Create New Release Note</h1>
+                        <div className="btnWrapper">
+                            <Button className="backBtn" variant="outline-primary" onClick={() => navigation(-1)}>
+                                Back
+                            </Button>
+                            <Button className="saveBtn" variant="outline-primary" onClick={() => navigation("")}>
+                                Save
+                            </Button>
+                        </div>
                     </div>
+                    <FloatingWrapper className="newRelaseNote" width="90%" borderRadius="25px">
+                        <div className="detailsWrapper">
+                            <div className="rnTag">AL-123</div>
+                            <h6>
+                                Version : V<input className="versionInput" placeholder={data.version} />
+                            </h6>
+                            <h6>
+                                Update Date : <input className="updateDateInput" placeholder={data.date} />
+                            </h6>
+                        </div>
+                        {categoryData.map((it) => (it.show ? <RNColumn columnId={it.key} tag={it.tag} key={it.key} content={it.content} data={data} setData={setData} /> : null))}
+                        {/* show 값이 true인 column만 렌더링 한다 */}
+                    </FloatingWrapper>
                 </div>
-                <FloatingWrapper className="newRelaseNote" width="90%" borderRadius="25px">
-                    <div className="detailsWrapper">
-                        <div className="rnTag">AL-123</div>
-                        <h6>
-                            Version : V<input className="versionInput" />.<input className="versionInput" />.<input className="versionInput" />.
-                        </h6>
-                        <h6>
-                            Update Date : <input className="updateDateInput" />
-                        </h6>
-                    </div>
-                    {data.map((it) => (it.show ? <RNColumn tag={it.tag} key={it.key} /> : null))}
-                    {/* show 값이 true인 column만 렌더링 한다 */}
-                </FloatingWrapper>
-            </div>
+            </FadeIn>
             <FloatingWrapper className="rightNavigation" width="220px" height="fit-content">
-                <h4>Add New Category</h4>
-                <div className="tagButton" onClick={() => toggleTag("new")}>
-                    <RNBadge tag={"new"} />
-                </div>
-                <div className="tagButton" onClick={() => toggleTag("featured")}>
-                    <RNBadge tag={"featured"} />
-                </div>
-                <div className="tagButton" onClick={() => toggleTag("changed")}>
-                    <RNBadge tag={"changed"} />
-                </div>
-                <div className="tagButton" onClick={() => toggleTag("fixed")}>
-                    <RNBadge tag={"fixed"} />
-                </div>
-                <div className="tagButton" onClick={() => toggleTag("deprecated")}>
-                    <RNBadge tag={"deprecated"} />
-                </div>
-                <div className="tagButton" onClick={() => toggleTag("bug")}>
-                    <RNBadge tag={"bug"} />
-                </div>
+                <FadeIn className="rightNavigationWrapper" childClassName="childClassName">
+                    <h4>Add New Category</h4>
+                    <div className="tagButton" onClick={() => toggleTag("new")}>
+                        <RNBadge tag={"new"} />
+                    </div>
+                    <div className="tagButton" onClick={() => toggleTag("featured")}>
+                        <RNBadge tag={"featured"} />
+                    </div>
+                    <div className="tagButton" onClick={() => toggleTag("changed")}>
+                        <RNBadge tag={"changed"} />
+                    </div>
+                    <div className="tagButton" onClick={() => toggleTag("fixed")}>
+                        <RNBadge tag={"fixed"} />
+                    </div>
+                    <div className="tagButton" onClick={() => toggleTag("deprecated")}>
+                        <RNBadge tag={"deprecated"} />
+                    </div>
+                    <div className="tagButton" onClick={() => toggleTag("bug")}>
+                        <RNBadge tag={"bug"} />
+                    </div>
+                </FadeIn>
             </FloatingWrapper>
         </div>
     );
--- src/pages/ReleaseNote/ReleaseNote.tsx
+++ src/pages/ReleaseNote/ReleaseNote.tsx
@@ -13,46 +13,68 @@ const Mock: ReleaseNoteData[] = [
         version: "V1.0.10",
         date: "2023.06.04",
         content: [
-            { tag: "new", content: ["Added homepage to allow user to show they are logged in", "Added a sort button allowing the user to filter the value array according to their needs."] },
-            { tag: "featured", content: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maecenas nec aenean a placerat vitae commodo."] },
-            { tag: "changed", content: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maecenas nec aenean a placerat vitae commodo."] },
-            { tag: "fixed", content: ["Adaptation of the languages ​​and texts of the commentary part to better reflect reality"] },
+            {
+                key: 0,
+                tag: "new",
+                content: [
+                    { key: 0, content: "Added homepage to allow user to show they are logged in" },
+                    { key: 1, content: "Added a sort button allowing the user to filter the value array according to their needs." },
+                ],
+            },
+            { key: 1, tag: "featured", content: [{ key: 0, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maecenas nec aenean a placerat vitae commodo." }] },
+            { key: 2, tag: "changed", content: [{ key: 0, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maecenas nec aenean a placerat vitae commodo." }] },
+            { key: 3, tag: "fixed", content: [{ key: 0, content: "Adaptation of the languages ​​and texts of the commentary part to better reflect reality" }] },
         ],
     },
     {
         version: "V1.0.9",
         date: "2023.06.04",
         content: [
-            { tag: "new", content: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maecenas nec aenean a placerat vitae commodo."] },
+            { key: 0, tag: "new", content: [{ key: 0, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maecenas nec aenean a placerat vitae commodo." }] },
             {
+                key: 1,
                 tag: "featured",
                 content: [
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maecenas nec aenean a placerat vitae commodo.",
-                    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maecenas nec aenean a placerat vitae commodo.",
+                    { key: 0, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maecenas nec aenean a placerat vitae commodo." },
+                    { key: 1, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maecenas nec aenean a placerat vitae commodo." },
                 ],
             },
-            { tag: "changed", content: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maecenas nec aenean a placerat vitae commodo."] },
-            { tag: "fixed", content: ["Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maecenas nec aenean a placerat vitae commodo."] },
+            { key: 2, tag: "changed", content: [{ key: 0, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maecenas nec aenean a placerat vitae commodo." }] },
+            { key: 3, tag: "fixed", content: [{ key: 0, content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In maecenas nec aenean a placerat vitae commodo." }] },
         ],
     },
     {
         version: "V1.0.8",
         date: "2023.06.04",
         content: [
-            { tag: "new", content: ["content"] },
-            { tag: "featured", content: ["content1", "content2"] },
-            { tag: "changed", content: ["content"] },
-            { tag: "fixed", content: ["content"] },
+            { key: 0, tag: "new", content: [{ key: 0, content: "content" }] },
+            {
+                key: 1,
+                tag: "featured",
+                content: [
+                    { key: 0, content: "content1" },
+                    { key: 0, content: "content2" },
+                ],
+            },
+            { key: 2, tag: "changed", content: [{ key: 0, content: "content" }] },
+            { key: 3, tag: "fixed", content: [{ key: 0, content: "content" }] },
         ],
     },
     {
         version: "V1.0.7",
         date: "2023.06.04",
         content: [
-            { tag: "new", content: ["content"] },
-            { tag: "featured", content: ["content1", "content2"] },
-            { tag: "changed", content: ["content"] },
-            { tag: "fixed", content: ["content"] },
+            { key: 0, tag: "new", content: [{ key: 0, content: "content" }] },
+            {
+                key: 1,
+                tag: "featured",
+                content: [
+                    { key: 0, content: "content1" },
+                    { key: 0, content: "content2" },
+                ],
+            },
+            { key: 2, tag: "changed", content: [{ key: 0, content: "content" }] },
+            { key: 3, tag: "fixed", content: [{ key: 0, content: "content" }] },
         ],
     },
 ];
@@ -83,7 +105,7 @@ export const ReleaseNote = () => {
                                         {it.content.map((it) => (
                                             <div className="content">
                                                 <BsDot width={18} />
-                                                <div className="text">{it}</div>
+                                                <div className="text">{it.content}</div>
                                             </div>
                                         ))}
                                     </div>
