ef0e80d2687570f6d53147e8f92d96db7f6659a6
chromato99 <chromato99@gmail.com>
2023-08-12 16:30:35 +0900

    update to handle each object sync individually

--- src/components/RNColumn.tsx
+++ src/components/RNColumn.tsx
@@ -7,7 +7,7 @@ import { Button } from "react-bootstrap";
 import { Dispatch, SetStateAction, useRef, useState } from "react";
 import { TextButton } from "./Buttons";
 import FadeIn from "../animation/FadeIn";
-import yorkie, { Text as YorkieText } from 'yorkie-js-sdk';
+import yorkie, { type Document as YorkieDoc } from 'yorkie-js-sdk';
 
 const RNColumnContent: RNColumnContentData[] = [{ key: 0, content: "" }];
 
@@ -15,61 +15,55 @@ export const RNColumn = ({
     columnId,
     tag,
     content,
+    doc,
     data,
-    // setData,
-    updateData,
+    setRender,
 }: {
     columnId: number;
     tag: RNTag;
     content: RNColumnContentData[];
     data: ReleaseNoteData;
-    // setData: Dispatch<SetStateAction<ReleaseNoteData>>;
-    updateData: (data: ReleaseNoteData) => void;
+    doc: YorkieDoc<ReleaseNoteData>;
+    setRender: Dispatch<SetStateAction<{}>>;
 }) => {
     const contentId = useRef(1);
 
     const deleteItem = (id: number) => {
-        const newContent = data.content.map((column) => {
-            if (column.key === columnId) {
-                const newColumnContentData = column.content.filter((item) => item.key !== id);
-                return { ...column, content: newColumnContentData };
+        doc.update((root) => {
+            for (let column = 0; column < data.content.length; column++) {
+                if (data.content[column].key === columnId) {
+                    const newColumnContentData = data.content[column].content.filter((item) => item.key !== id);
+                    root.content[column].content = newColumnContentData;
+                    contentId.current -= 1;
+                    setRender({});
+                    break;
+                }
             }
-            return column;
-        });
-        updateData({ ...data, content: newContent });
+        }, `delete column item`);
     };
+
     const addItem = () => {
-        const newColumnContentData: RNColumnContentData = { key: contentId.current, content: "" };
-        const columnContentData: RNColumnContentData[] = [...data.content[columnId].content, newColumnContentData];
-        const newColumnData: ReleaseNoteColumnData = data.content[columnId];
-        newColumnData.content = columnContentData;
-
-        const newContentData = data.content.map((column) => {
-            if (column.key === columnId) {
-                return newColumnData;
-            }
-            return column;
-        });
-
-        contentId.current += 1;
-        updateData({ ...data, content: newContentData});
+        doc.update((root) => {
+            const newColumnContentData: RNColumnContentData = { key: contentId.current, content: "" };
+            root.content[columnId].content.push(newColumnContentData);
+            contentId.current += 1;
+            setRender({});
+        }, `add column item`);
     };
 
     const handleInputChange = (itemId: number, inputValue: string) => {
-        const newContentData = data.content.map((column) => {
-            if (column.key === columnId) {
-                const newColumnContentData = column.content.map((item) => {
-                    if (item.key === itemId) {
-                        return { ...item, content: inputValue };
+        doc.update((root) => {
+            for (let column = 0; column < data.content.length; column++) {
+                if (data.content[column].key === columnId) {
+                    for (let item = 0; item < data.content[column].content.length; item++) {
+                        if (data.content[column].content[item].key === itemId) {
+                            root.content[column].content[item].content = inputValue;
+                            setRender({});
+                        }
                     }
-                    return item;
-                });
-                return { ...column, content: newColumnContentData };
+                }
             }
-            return column;
-        });
-
-        updateData({ ...data, content: newContentData });
+        }, 'update column item');
     };
     return (
         <FloatingWrapper className="RNColumn" borderRadius="20px">
--- src/pages/ReleaseNote/CreateReleaseNote.tsx
+++ src/pages/ReleaseNote/CreateReleaseNote.tsx
@@ -4,11 +4,10 @@ import "./CreateReleaseNote.css";
 import { RNBadge } from "../../components/RNBadge";
 import { FloatingWrapper } from "../../components/FloatingWrapper";
 import { RNColumn } from "../../components/RNColumn";
-import { RNTag, ReleaseNoteColumnData, ReleaseNoteData } from "../../interfaces/releaseNote.interface";
+import { RNColumnContentData, RNTag, ReleaseNoteColumnData, ReleaseNoteData } from "../../interfaces/releaseNote.interface";
 import { useRef, useState, useEffect } from "react";
 import FadeIn from "../../animation/FadeIn";
 import yorkie, { Text as YorkieText, OperationInfo } from 'yorkie-js-sdk';
-import { ReleaseNote } from "./ReleaseNote";
 
 const yorkieApiURL: string = process.env.REACT_APP_YORKIE_API_URL!;
 const yorkieApiKey: string = process.env.REACT_APP_YORKIE_API_KEY!;
@@ -28,48 +27,29 @@ const initialDisplayData: ReleaseNoteData = {
 
 export const CreateReleaseNote = () => {
   const navigate = useNavigate();
-  const [data, setData] = useState(initialDisplayData);
+  const [render, setRender] = useState({});
   const [client, setClient] = useState(new yorkie.Client(yorkieApiURL, { apiKey: yorkieApiKey }));
   const [doc, setDoc] = useState(new yorkie.Document<ReleaseNoteData>(
     // `release-note-${Math.floor(Math.random() * 100000)}`,
-    `release-note-37`,
+    `release-note-42`,
   ));
 
-  interface ContentDetail {
-    key: number;
-    content: string;
-  }
-
-  interface ContentItem {
-    key: number;
-    tag: string;
-    content: ContentDetail[];
-    show: boolean;
-  }
-
   const toggleTag = (tag: RNTag) => {
-    let filteredData = { ...data };
-    const filteredContentData = data.content.map((it) => (it.tag === tag ? { ...it, show: !it.show } : it));
-
-    console.log(filteredContentData);
-    filteredData.content = [ ...filteredContentData ];
-    updateData(filteredData);
+    doc.update((root) => {
+      console.log("log: update toggle");
+      for (let column = 0; column < root.content.length; column++) {
+        if (root.content[column].tag === tag) {
+          root.content[column].show = !root.content[column].show;
+          break;
+        }
+      }
+      setRender({});
+    }, 'update toggle');
   };
 
   const onSaveRelaseNote = () => {
     navigate("/releasenote");
   };
-
-  const updateData = (newData: ReleaseNoteData) => {
-    setData(newData);
-    doc.update((root) => {
-      console.log("log: update data");
-      root.date = newData.date;
-      root.version = newData.version;
-      root.content = [...newData.content];
-    }, 'update data');
-  }
-
 
   useEffect(() => {
     client.activate().then(() => {
@@ -81,51 +61,28 @@ export const CreateReleaseNote = () => {
       });
 
       client.attach(doc).then(() => {
-
-        const displayData = () => {
-          console.log("display data")
-          const root = doc.getRoot();
-          console.log(root);
-          setData(() => {
-            const newColumnData = root.content.map((column) => {
-              const newColumnContentData = column.content.map((item) => {
-                const newColumnContent = {
-                  issue: item.issue ? item.issue : "",
-                  content: item.content,
-                  key: item.key,
-                }
-                return newColumnContent;
-              });
-              return { ...column, content: newColumnContentData };
-            });
-            return { ...root, content: newColumnData };
-          });
-        };
-
         // 02-2. subscribe document event.
         doc.update((root) => {
           console.log(root);
-          if (!root.version) {
+          if (!root.content) {
             console.log('log: create content if not exists');
             // root = initialData;
             root.date = "";
             root.version = "";
-            root.content = [...data.content];
-          } else {
-            console.log('log: content exists');
-            displayData();
+            root.content = [...initialDisplayData.content];
           }
+          setRender({});
         }, 'create content if not exists');
 
         doc.subscribe((event) => {
           if (event.type === 'snapshot') {
             // The text is replaced to snapshot and must be re-synced.
             console.log(event);
-            displayData();
+            setRender({});
           } else if (event.type === 'remote-change') {
             // The text is updated by remote changes.
             console.log(event);
-            displayData();
+            setRender({});
           } else if (event.type === 'local-change') {
             console.log(event);
           }
@@ -154,24 +111,30 @@ export const CreateReleaseNote = () => {
             <div className="detailsWrapper">
               <div className="rnTag">AL-123</div>
               <h6>
-                Version : <input className="versionInput" value={data.version} onChange={(e) => {
-                  updateData({ ...data, version: e.target.value })
+                Version : <input className="versionInput" value={doc.getRoot().version} onChange={(e) => {
+                  doc.update((root) => {
+                    root.version = e.target.value;
+                    setRender({});
+                  });
                 }} placeholder={"V0.0.0"} />
               </h6>
               <h6>
                 Update Date :{" "}
                 <input
                   className="updateDateInput"
-                  value={data.date}
-                  onChange={async (e) => {
-                    updateData({ ...data, date: e.target.value })
+                  value={doc.getRoot().date}
+                  onChange={(e) => {
+                    doc.update((root) => {
+                      root.date = e.target.value;
+                      setRender({});
+                    });
                   }}
                   placeholder={"YYYY-MM-DD"}
                 />
               </h6>
             </div>
-            {data.content.map((it) =>
-              it.show ? <RNColumn columnId={it.key} tag={it.tag} key={it.key} content={it.content} data={data} updateData={updateData} /> : null
+            {doc.getRoot().content && doc.getRoot().content.map((it) =>
+              it.show ? <RNColumn columnId={it.key} tag={it.tag} key={it.key} content={it.content} data={doc.getRoot()} doc={doc} setRender={setRender} /> : null
             )}
             {/* show 값이 true인 column만 렌더링 한다 */}
           </FloatingWrapper>
